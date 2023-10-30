9dba1e45cb803943de1e3d70de3af766a3d95992
rizqyn9 <54632504+rizqyn9@users.noreply.github.com>
2023-10-30 11:12:31 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ app/page.tsx
@@ -0,0 +1,137 @@
+import Image from "next/image"
+
+export default function Home() {
+  return (
+    <main className="p-5 text-lg font-semibold leading-10 text-yellow-200 flex flex-col gap-5">
+      <h1 className="text-2xl mt-5">Sayang ntar dibaca ya 🫶</h1>
+      <p className="py-4">------------------------------------</p>
+      <p className="indent-4">
+        Hari ini aku gagal pergi sayang, ternyata kepergianku ninggalin luka
+        buat sayang, aku disini makin bingung. Sayang semangat ya !!! Aku minta
+        maaf keputusan ku gk pikir panjang.
+      </p>
+      <p className="py-4">------------------------------------</p>
+      <p className="indent-4">
+        Sebelumnya aku minta maaf ya sayang, aku minta maaf sebesar besarnya,
+        aku gak ada pilihan sayang,
+      </p>
+      <p className="indent-4">
+        Kemarin aku ketemu sama orang suruhan mereka, mereka menaruh tawanan
+        keselamatan atas sayang dan orang2 yg aku sayang, aku masih berusaha
+        buat melindungi itu semua, tapi saat ini sayang lagi terancam, aku gak
+        tau harus gimana lagi, notaris ku yang bner2 kubuat komunikasi terbatas
+        ngebuat aku juga susah buat kasih nama sayang ke notaris itu, mungkin
+        ini satu2 nya kunci sayang dan aku bisa sama2 hidup tenang
+      </p>
+      <p className="indent-4">
+        Hari ini bener2 aku gak tau arah lagi, sayang jauh di rumah, aku pengen
+        cerita bnyak hal, ditambah sayang dah tah aku dikeluarin dari kerjaan.
+        Aku takut aku gk bisa penuhin mimpi2 sayang. Tapi makasih loh buat
+        sayang, sayang dah semangatin aku, aku nangis waktu baca chat sayang.
+        Hehehehre. Tapi sayqng tau ndak klo kontrakku ternyata di ambil sama
+        pihak ggg nih.
+      </p>
+      <p className="indent-4">
+        Sayang ntar klo aku ambil pilihan buat ngorbanin aku sayang harus bisa
+        kuat ya, pasti ntar datang orang yg lebih baik dari aku, tapi sayang
+        harus yakin gak ada orang yg lebih sesayang aku, mungkin hal ini bakal
+        jadi hal yg berat buat sayang kita dah bentuk pengalaman2 baru
+        bebarengan, tapi Bismillah kebahagiaan sayang bakal terpenuhi lagi
+        dengan orang yang baru,
+      </p>
+      <p className="indent-4">
+        Sayang gk usah khawatir soal pekerjaan ya, aku bakal cari orang2 yg siap
+        bantu sayang. Ternyata aku punya banyak temen yang emang sayang loh sama
+        aku, mereka setia bnget buat bantu orang2 yg aku sayang. Speechless 😶.
+      </p>
+      <p className="indent-4">
+        Oh ya sayang aku tuh dah beli beberapa tiket liburan aku belum kasih tau
+        sayang nih, belum lunas juga sih hehehe :( , tapi ntar sayang bisa kok
+        pakai uang2 ku. Sayang ntar liburan nya ajak temen yaaa
+      </p>
+      <p className="indent-4">
+        Aku suka bnget ketika liburan sama sayang, meskipun setiap jalan aku
+        selalu kecewain sayang :( maaf ya sayang aku gak ada kesengajaan buat
+        kecewain sayang
+      </p>
+      <p className="indent-4">
+        Sayang, makasih bnyak ya aku tuh trauma bnget tauk ke pantai. Tapi
+        berkat sayang aku jadi berani hehehe. Sayang klo kepantai jangan sendiri
+        yaa.
+      </p>
+      <p className="indent-4">
+        Sayang ntar bakal ada dokter yang nemenin sayang, dokter ini khusus buat
+        aku, ntar aku minta jaga sayang, sayang pola makannya dijaga dong, kamu
+        gak mau kan ngeliat aku sedih?? Sayang klo makan juga dijaga jangan
+        kebanyakan, gk baik buat kesehatan. Sayang ntar klo menikah rawat
+        pasangannya yg sabar ya, terus klo punya anak ntar di perhatiin kasih
+        semua yg sayang punya ke anak sayang.
+      </p>
+      <p className="indent-4">
+        Ada lagi nih, aku tuh pasang beberapa asuransi ku di nama sayang, ntar
+        klo aku kenapa2 uang itu bisa dipakai sayang ya buat kejar mimpi2 nya.
+        Emang gk banyak sihh tapi sedikit bisa membantu. Nomor polis ntar ada di
+        brankas ya,
+      </p>
+      <p className="indent-4">
+        Perihal brankas ntar sayang datang aja ke BCA kudus, terus kasih tau
+        detail data diri ku kesana ya, brankas ini yang tau cuman aku, sekarang
+        aku titipkan ke sayang. Disana juga ada beberapa emas, uang, sayang
+        semisal mau pakai gak apa, klo gak mau pakai ntar sumbangin ya keorang
+        yg bner2 butuh. Oh ya jangan diambil semua ya sayang, ntar notaris ku
+        bakal bagiin kok, yang penting kamu kasih pin nya ya.
+      </p>
+      <p className="indent-4">
+        Kamu cek blu bca ku ya, aku ada tabungan buat nikah besarnya masih di
+        25jt. Ntar sayang bisa pakai buat tabungan sayang nikah. Aku bersyukur
+        bnget klo sayang mau pakai itu.
+      </p>
+      <p className="indent-4">
+        Sayang jangan terlalu dengerin omongan orang, tau baik buruknya itu
+        cuman diri sayang yg tau. Sayang dah dewasa dan dah sabar banget. Aku
+        bangga banget loh sama sayang, bahkan aku sering minder sayang kok
+        berubah nya secepet itu. :(
+      </p>
+      <p className="indent-4">
+        Aku masih banyak yg pengen diobrolin, cuman ntar ya klo kamu dah
+        dijogja. Aku pengen dua malam itu jadi hari yang berkesan buat kita. Aku
+        pengen semisal aku emng udah gak ada aku ninggalin suatu kebahagiaan.
+        Aku juga dah nulis ini buat mamah papah adek, mereka kira2 mau baca ndak
+        ya.
+      </p>
+      <p className="indent-4">
+        Oh ya aku tuh gk berharap aku gak ada, cuman aku berfikir terburuknya.
+        Semisal emang ntar aku baik2 aja, aku langsung jemput sayang, aku bakal
+        peluk sayang sedalem dalemnya, dan aku janji ketika sama sayang aku
+        bakal jadi pribadi yg lebih berhati hati.
+      </p>
+      <p className="indent-4">
+        Perihal pernikahan sayang jangan takut, apa yg terjadi dikeluarga sayang
+        gak akan mungkin terjadi sama keluarga sayang. Sayang itu cantik baik
+        perhatian pasti suami sayang bakal sayang banget sama sayang. Sayang pun
+        sama harus sayang banget ya sama dia, janji ke aku kamu bakal rawat
+        pasanganmu sebaik mungkin. Sayang pasti bisa !!!
+      </p>
+      <p className="indent-4">
+        Dah sayang aku cuman bisa ungkapin sampai disini aja, habis ini sayang
+        mau lupain aku gak masalah. Aku janji aku disamping sayang terus
+        menerus, aku juga selalu berdoa kan aku dah dirumah tuhan harusnya doaku
+        cepet kan dikabulkanNya, aku bakal berdoa klo semua orang yg aku sayang
+        mereka selalu mendapat kebahagiaan atas diri mereka, dah sayang
+        Terimakasih ya buat waktunya.
+      </p>
+      <p>SAYANG JANGAN MENYESALI PERTEMUAN KITA YA, 🫶🫶🫶🫶</p>
+      <div>
+        <p>Brankas BCA a/n Rizqy Prastya Ari Nugroho</p>
+        <p>
+          <span>PIN:</span>
+          <span>26122018</span>
+        </p>
+        <p>rizqynugroho9@gmail.com</p>
+      </div>
+      <p className="text-center">
+        This button can be oppened if the author was die :(
+      </p>
+    </main>
+  )
+}
